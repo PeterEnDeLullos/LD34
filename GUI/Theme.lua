76a388757fb61d9b1828e2f92295ae062d417b79
Matthias <boekewurm@gmail.com>
2015-12-13 17:17:10 +0100

    moved menu states to the states subfolder

--- GUI/Theme.lua
+++ GUI/Theme.lua
@@ -2,14 +2,9 @@ local Theme = {}
 
 Theme.Button = {}
 
-function Theme.Button.draw(self)
-    love.graphics.setColor(pColor())
-    if self.down then love.graphics.setColor(pColorHover()) end
-    love.graphics.rectangle('fill', self.x, self.y, self.w, self.h)
-    love.graphics.setColor(255,255,255)
+function Theme.bgColor()
+    return 242, 242, 223
 end
-
-function Theme.
 
 local function pColor()
     return 0, 0, 0
@@ -17,6 +12,17 @@ end
 
 local function pColorHover()
     return 10, 10, 10
+end
+
+function Theme.Button.draw(self)
+    love.graphics.setColor(pColor())
+    if self.down then love.graphics.setColor(pColorHover()) end
+    love.graphics.rectangle('fill', self.x, self.y, self.w, self.h)
+    love.graphics.setColor(255,255,255)
+end
+
+function Theme.update(self)
+
 end
 
 return Theme
